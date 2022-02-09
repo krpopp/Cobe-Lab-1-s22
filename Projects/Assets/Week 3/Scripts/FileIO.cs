de85dfb422fc1d433c5916812f9fbb89fa2b8243
karina <>
2022-02-09 12:34:15 -0500

    adding week 3 comments

--- Projects/Assets/Week 3/Scripts/FileIO.cs
+++ Projects/Assets/Week 3/Scripts/FileIO.cs
@@ -1,28 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.IO;
+using System.IO; //to write to a file, we need the System.IO library
 
 public class FileIO : MonoBehaviour
 {
+    //the name of our file
+    //we're making it a const because it's a variable that really never needs to change
+    //you must include the file ending
     const string FILE_NAME = "Week3Save.txt";
 
     // Start is called before the first frame update
     void Start()
     {
+        //open the stream writer with the file we want to write to
+        //if the file doesn't exist, the stream writer will create it
+        //the bool at the end decides if we apend to the file
+        //false == overwrite the file
+        //true == add to the file
         StreamWriter writer = new StreamWriter(FILE_NAME, false);
+        //write to the file
         writer.Write("hello i just wrote to this text file yay!");
+        //close the stream writer
         writer.Close();
 
+        //open the stream reader
         StreamReader reader = new StreamReader(FILE_NAME);
+        //read the line the stream is on
+        //lines end with /n in text
+        //(we'll go over this in week 4)
         string content = reader.ReadLine();
+        //print the line to the console
         Debug.Log(content);
+        //close the reader
         reader.Close();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
