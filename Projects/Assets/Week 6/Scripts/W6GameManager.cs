20c7cdd0b2298a51f6a0cc6636f26156541715d4
Karina <karina.popp@nyu.edu>
2022-03-17 12:51:38 -0400

    Add week 6 code

--- Projects/Assets/Week 6/Scripts/W6GameManager.cs
+++ Projects/Assets/Week 6/Scripts/W6GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class W6GameManager : MonoBehaviour
 {
@@ -18,12 +19,21 @@ public class W6GameManager : MonoBehaviour
     //all potential game states
     public enum State {
         CreateCards,
+        Deal,
         Select,
         CardChoosen,
         Resolve
     }
 
     public State currentState; //tracks the state the game is on now
+
+    List<GameObject> deck = new List<GameObject>();
+
+    public Vector3 handStartPos;
+
+    public float enemyHealth;
+    public Text enemyText;
+
 
     void Awake() {
         //singleton 
@@ -40,13 +50,14 @@ public class W6GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        enemyText.text = enemyHealth.ToString();
         TransitionStates(State.CreateCards); //start by creating cards
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RunState();
     }
 
     public void TransitionStates(State newState)
@@ -59,13 +70,16 @@ public class W6GameManager : MonoBehaviour
                 {
                     GameObject newCard = Instantiate(cardObj); //make a card
                     Vector3 newPos = new Vector3( //make a new position
-                        gameObject.transform.position.x + (i * 3),
+                        gameObject.transform.position.x,
                         gameObject.transform.position.y,
                         0);
                     newCard.transform.position = newPos; //set the card's position
+                    deck.Add(newCard);
                 }
                 //once all the cards have been made
-                TransitionStates(State.Select); //go to the selection state
+                TransitionStates(State.Deal); //go to the selection state
+                break;
+            case State.Deal:
                 break;
             case State.Select:
                 break;
@@ -75,6 +89,24 @@ public class W6GameManager : MonoBehaviour
                 break;
             default:
                 Debug.Log("this state doesn't exist");
+                break;
+        }
+    }
+
+    void RunState() {
+        switch (currentState)
+        {
+            case State.Deal:
+                for(int i = 0; i < deck.Count; i++)
+                {
+                    float step = 5.0f * Time.deltaTime;
+                    Vector3 newPos = new Vector3(handStartPos.x + (i * 3), handStartPos.y, 0);
+                    deck[i].transform.position = Vector3.MoveTowards(deck[i].transform.position, newPos, step);
+                    if(i == deck.Count - 1 && Vector3.Distance(deck[i].transform.position, newPos) < 0.1f)
+                    {
+                        TransitionStates(State.Select);
+                    }
+                }
                 break;
         }
     }
